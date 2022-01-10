d6fd8dd1beee937079bcc992ff84313d4a80128a
Abe210 <you@example.com>
2022-01-10 02:57:27 +0100

    just saving

--- hashfinal.c
+++ hashfinal.c
@@ -4,18 +4,14 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <errno.h>
-#include <limits.h>
 
-#define MAX_NAME 256
-#define TABLE_SIZE 4000000
-#define MAXCHAR 100
-
+#define TABLE_SIZE 300000
 #define MAX_NAME 256 //Longueur maximale d'un Nom
 // DEFINITION D'UN TYPE PERSON : a person
 // Element: un prénom unique (Ex: ALain=Alain mais Alain!=Marc)
 //min_w = "Minimum year of appearance" 
 struct person{
-    char *name; //Hash key
+    char name[MAX_NAME]; //Hash key
     int sex[2]; //sex[0]=nb_women & sex[1]=nb_men => nb_total = sex[0] + sex[1]
     int birthdate[4]; // birthdate[0]=min_w, birthdate[1]=max_w, birthdate[2]=min_m & birthdate[3]=max_m
     struct person *next;
@@ -23,39 +19,38 @@ struct person{
 typedef struct person person;
 
 //List
-typedef struct person *Liste;
-Liste liste_vide = (Liste)NULL;
-
 //Array of List 
 //A person can be easily found by index = hash("name")
 // -During collision, if hash("Name") = hash("Name1") then the person is 
 //  chained on the List otherwise the List is in front
-typedef Liste *HashTable;
-HashTable hash_vide = (HashTable)NULL;
+typedef struct person *Liste;
+Liste liste_vide = (Liste)NULL;
+
 
 Liste create_person(char* name, int sexe, int birthdate,int number) {
     // Creates a pointer to a new hash table person item
-    Liste ptrperson = malloc (sizeof(person));
-    ptrperson->name = malloc (strlen(name)+1);
+    Liste ptrperson = malloc(sizeof(person));
+    if (ptrperson == NULL) {
+        printf("Error! memory not allocated.");
+        exit(-1);
+    }
     strcpy(ptrperson->name, name);
     switch (sexe)
     {
         case 1: //it is a woman
             ptrperson->sex[0] = number;
-            ptrperson->sex[1] = 0;
             ptrperson->birthdate[0] = birthdate;
             ptrperson->birthdate[1] = birthdate;
             break;
         case 2: //it is a man
             ptrperson->sex[1] = number;
-            ptrperson->sex[0] = 0;
             ptrperson->birthdate[2] = birthdate;
             ptrperson->birthdate[3] = birthdate;
         default:
             //printf("Error format - Column 1");
             break;
     }
-    ptrperson->next = NULL;
+    //ptrperson->next = NULL;
     return ptrperson;
 }
 bool EstVide(Liste L) { return L == liste_vide ? true : false; }
@@ -65,7 +60,7 @@ Liste GetSucc(Liste L) {
   return L->next;
 }
 //Display a person
-void displayPerson(Liste L){
+void displayList(Liste L){
     Liste tmpL = L;
     while (EstVide(tmpL) == false) {
         printf("%i\t%s\t%i -- ",tmpL->sex[0], tmpL->name, tmpL->birthdate[0]);
@@ -77,7 +72,7 @@ void displayPerson(Liste L){
 //Return 0 if error, the converted number otherwise
 int ConvertToInt(char *s){
     char * pEnd;
-    const long sl = strtol(s, &pEnd, 10);
+    long sl = strtol(s, &pEnd, 10);
     errno = 0;
     if (pEnd == s) {
         //fprintf(stderr, "%s: not a decimal number\n", s);
@@ -88,7 +83,6 @@ int ConvertToInt(char *s){
     } else {
         return (int)sl;
     }
-    
 }
 
 //Returns the index for the table of a specific person based on its name (name is the key) 
@@ -102,15 +96,15 @@ unsigned int hash(char *name){
     return hash;
 }
 
-HashTable init_hash_table(HashTable hashtable){
+Liste init_hash_table(Liste *hashtable){
     for(int i=0; i < TABLE_SIZE; i++){
         hashtable[i] = NULL;
     }
-    return hashtable;
+    return hashtable[0];
 }
 
-void print_table(HashTable hash_table){
-    Liste L = NULL;
+void print_table(Liste *hash_table){
+    Liste L;
     for(int i=0; i < TABLE_SIZE; i++){
         L = hash_table[i];
         if(L == NULL){
@@ -118,14 +112,14 @@ void print_table(HashTable hash_table){
         }else{
             while(!EstVide(L)){
                 printf("%i) %i %i %s %i %i %i %i --> ",i, L->sex[0], L->sex[1], L->name, L->birthdate[0],L->birthdate[1],L->birthdate[2],L->birthdate[3]);
-                L = L->next;
+                L = GetSucc(L);
             }
             printf("\n");
         }
     }
 }
 
-void free_table(HashTable hash_table){
+void free_table(Liste *hash_table){
     Liste Lcurrent = NULL;
     Liste Lnext = NULL;
     for(int i=0; i < TABLE_SIZE; i++){
@@ -140,46 +134,47 @@ void free_table(HashTable hash_table){
 }
 
 //find an Element in the table by their name (key)
-Liste hash_search(HashTable hash_table, Liste L){
+Liste hash_search(Liste *hash_table, Liste L){
     int i = hash(L->name);
-    Liste tmpl = hash_table[i];
-    while(!EstVide(tmpl) && strcmp(tmpl->name,L->name)!=0){
-        tmpl = GetSucc(tmpl);
+    Liste tmph = hash_table[i];
+    while(!EstVide(tmph) && strcmp(tmph->name,L->name)!=0){        
+        tmph = GetSucc(tmph);
     }
-    return tmpl;
+    return tmph;
 }
 
-bool hash_table_insert(HashTable hash_table, Liste L){
+bool hash_table_insert(Liste *hash_table, Liste L){
     if (EstVide(L)) return false;
     int i = hash(L->name);
-    Liste tmpl=NULL;
+    Liste tmpl = NULL;
     if (hash_table[i] != NULL) //collision
     {
-        tmpl = hash_search(hash_table,L);
+        tmpl = hash_search(hash_table,L); //Search if Person already exist
         if (tmpl != NULL){ //Found
             if (L->sex[0] > 0)
             {
                 tmpl->sex[0] = tmpl->sex[0] + L->sex[0];
                 if (L->birthdate[0] < tmpl->birthdate[0] && L->birthdate[0] != 0) //datebirth check if less than min_date_of_birth for women
                 {
-                    tmpl->birthdate[0] = L->birthdate[0]; 
+                    hash_table[i]->birthdate[0] = L->birthdate[0]; 
                 }
                 if (L->birthdate[1] > tmpl->birthdate[1]) //datebirth check if greater than max_date_of_birth for women
                 {
-                    tmpl->birthdate[1] = L->birthdate[1];
+                    hash_table[i]->birthdate[1] = L->birthdate[1];
                 }
             }else if (L->sex[1] > 0)
             {
-                tmpl->sex[1] = tmpl->sex[0] + + L->sex[1];
+                hash_table[i]->sex[1] = hash_table[i]->sex[0] + L->sex[1];
                 if (L->birthdate[2] < tmpl->birthdate[2] && L->birthdate[0] != 0) //datebirth check if less than min_date_of_birth for men
                 {
-                    L->birthdate[2] = tmpl->birthdate[2]; 
+                    hash_table[i]->birthdate[2] = tmpl->birthdate[2]; 
                 }
                 if (L->birthdate[3] > tmpl->birthdate[3]) //datebirth check if greater than max_date_of_birth for men
                 {
-                    L->birthdate[3] = tmpl->birthdate[3];
+                    hash_table[i]->birthdate[3] = tmpl->birthdate[3];
                 }
             }
+            free(L); // data saved so we don't need this allocated space
             return true; 
         }
     }
@@ -190,7 +185,7 @@ bool hash_table_insert(HashTable hash_table, Liste L){
 
 //Read the CSV and store into appropriate structure
 //second argument HashTable *ht
-void readCSV(char *filename,HashTable hash_table){
+void readCSV(char *filename,Liste *hash_table){
     //arguments (File *fp, Liste Hashtable[SIZE], char *fileName)
 
     FILE *fp;
@@ -201,7 +196,6 @@ void readCSV(char *filename,HashTable hash_table){
     }
 
     // Process Data here
-    Liste tmp = NULL;
     char *b = NULL;
     size_t bufsize = 50;
     b = malloc(bufsize * sizeof(char));
@@ -223,11 +217,9 @@ void readCSV(char *filename,HashTable hash_table){
             switch (i){
                 /* for every element we store it in appropriate type */
                 case 0: //Convert sexe and year to int
-                    s = ConvertToInt(token); // gère le cas ou token est invalide
+                    s = ConvertToInt(token); // gère le cas ou token est invalide return 0 if error
                     break;
                 case 1: //store name as string
-                    //bool x = hash_table_insert(hashtable,);
-                    //printf("hash_table[6]->data->name = %s\n",hash_table->sex hash_table[6]->data->name);
                     prenom = token;
                     break;
                 
@@ -245,18 +237,18 @@ void readCSV(char *filename,HashTable hash_table){
                     break;
             }
         }
-        tmp = create_person(prenom,s,annee,nb);
-        hash_table_insert(hash_table,tmp);
+        Liste ret = create_person(prenom,s,annee,nb);
+        hash_table_insert(hash_table,ret);
     }
         fclose(fp);
 }
 
 //User menu: choice 0 to 4
-void menu(HashTable hashtable){ //param: HashTable *ht,
+void menu(Liste *hashtable){ //param: HashTable *ht,
   int total=0;
   int choix = -1;
   char choix1 = 'x';
-  char prenom[MAXCHAR];
+  char prenom[MAX_NAME];
   while (choix != 4)
   {
     printf("Que voulez-vous afficher ? (0 pour le menu) > ");
@@ -290,21 +282,23 @@ void menu(HashTable hashtable){ //param: HashTable *ht,
 
 int main(int argc, char *argv[]){
 
-    char filename[100] = "Test.csv";
-    //char filename[100] = "dpt2020.csv";
-    HashTable hashtable = calloc(TABLE_SIZE, sizeof(Liste));
-    //make table "heads" NULL
-    for(int i = 0; i < TABLE_SIZE; i++){
-    	hashtable[i] = NULL;
+    //Liste hashtable[TABLE_SIZE];  /* create CONST number of pointers to type */
+    Liste *hashtable = calloc(TABLE_SIZE, sizeof(Liste));
+
+    if (hashtable == NULL) {
+        printf("Error! memory not allocated.");
+        exit(-1);
     }
     printf("TABLE INITIALIZED! \n\n");
 
+    char filename[100] = "Test.csv";
+    //char filename[100] = "dpt2020.csv";
+    //make table "heads" NULL
+    
     readCSV(filename,hashtable);
-    //print_table(hashtable);
-    person person = {.name="Justine"};
-    Liste ptr = &person;   
-    Liste someone = hash_search(hashtable,ptr);
-    printf("Nom: %s %i %i %i %i",someone->name, someone->birthdate[0], someone->birthdate[1], someone->birthdate[2], someone->birthdate[3]);
+    menu(hashtable);
+    print_table(hashtable);
+
     // print_table();
     free_table(hashtable);
 
